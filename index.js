fa9fbecb10ba9815e53130f28baf76f6d4f6f3f1
Ran Ramati <ran@logz.io>
2017-06-09 10:05:32 +0300

    Create index.js

--- index.js
+++ index.js
@@ -56,9 +56,11 @@ function start(opts) {
         else if (obj.type) {
             obj.action_type = obj.type;
             type = 'docker-events';
+            obj.app_id = process.env.MARATHON_APP_ID;
         }
         else if (obj.stats) {
             type = 'docker-stats';
+            obj.app_id = process.env.MARATHON_APP_ID;
         }
         obj.host = os.hostname();
 
